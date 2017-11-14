3eae99a18b5f0f48ccb38d22a1ab813e56b42d4b
Thibaud Marcé <thibaud.marce@gmail.com>
2017-11-14 11:51:40 -0500

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :photos, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy]
+
+  resources :users, only: [:show] do
+    resources :photos, only: [:index]
+    resources :favorites, only: [:index]
+  end
+
+  resources :spots, except: [:destroy] do
+    resources :reviews, only: [:create, :update, :destroy]
+  end
 end
