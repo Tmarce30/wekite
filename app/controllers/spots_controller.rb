466bb2ad23bc8a492a7f06582b180a7029571855
Thibaud Marcé <thibaud.marce@gmail.com>
2017-11-14 15:43:01 -0500

    added column photo to spots

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,11 +1,21 @@
 class SpotsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
   end
 
   def new
+    @spot = Spot.new
   end
 
   def create
+    @spot = Spot.new(spot_params)
+    @spot.user = current_user
+    if @spot.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
   end
 
   def show
@@ -16,4 +26,15 @@ class SpotsController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def set_puppy
+    @spot = Spot.find(params[:id])
+  end
+
+  def puppy_params
+    params.require(:spot).permit(:name, :breed, :photo, :address, :description)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114171516) do
+ActiveRecord::Schema.define(version: 20171114204103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20171114171516) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "photo"
     t.index ["user_id"], name: "index_spots_on_user_id", using: :btree
   end
 
