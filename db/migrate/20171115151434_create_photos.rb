34de6c5508c8970526dff331ec219e182e75f081
Thibaud Marcé <thibaud.marce@gmail.com>
2017-11-15 10:33:49 -0500

    migrated schema photo

--- db/migrate/20171115151434_create_photos.rb
+++ db/migrate/20171115151434_create_photos.rb
@@ -1,8 +1,8 @@
 class CreatePhotos < ActiveRecord::Migration[5.0]
   def change
     create_table :photos do |t|
-      t.references :user_id, foreign_key: true
-      t.references :spot_id, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :spot, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114204019) do
+ActiveRecord::Schema.define(version: 20171115151434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 20171114204019) do
     t.datetime "updated_at", null: false
     t.index ["spot_id"], name: "index_favorites_on_spot_id", using: :btree
     t.index ["user_id"], name: "index_favorites_on_user_id", using: :btree
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "spot_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["spot_id"], name: "index_photos_on_spot_id", using: :btree
+    t.index ["user_id"], name: "index_photos_on_user_id", using: :btree
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -89,6 +98,8 @@ ActiveRecord::Schema.define(version: 20171114204019) do
 
   add_foreign_key "favorites", "spots"
   add_foreign_key "favorites", "users"
+  add_foreign_key "photos", "spots"
+  add_foreign_key "photos", "users"
   add_foreign_key "reviews", "spots"
   add_foreign_key "reviews", "users"
   add_foreign_key "spots", "users"
