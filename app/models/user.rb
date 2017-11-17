e431bbce3b287b2fe9090a0b91ca108b7e7a7e40
Severine Clement <severine_clement@hotmail.fr>
2017-11-17 16:15:06 -0500

    remove age

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :age, presence: true
+  validates :age
   validates :email, uniqueness: true, presence: true
   validates :avatar, presence: true
 
