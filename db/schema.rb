af8527dce97e644f76ca09ad359bc81988d597b5
Thibaud Marcé <thibaud.marce@gmail.com>
2017-11-18 18:10:52 -0500

    created weather model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115204107) do
+ActiveRecord::Schema.define(version: 20171118230907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,6 +99,18 @@ ActiveRecord::Schema.define(version: 20171115204107) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "weathers", force: :cascade do |t|
+    t.integer  "spot_id"
+    t.string   "weather_desc"
+    t.string   "air_temp"
+    t.string   "wind_speed"
+    t.string   "wind_dir"
+    t.string   "water_temp"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["spot_id"], name: "index_weathers_on_spot_id", using: :btree
+  end
+
   add_foreign_key "favorites", "spots"
   add_foreign_key "favorites", "users"
   add_foreign_key "pictures", "spots"
@@ -106,4 +118,5 @@ ActiveRecord::Schema.define(version: 20171115204107) do
   add_foreign_key "reviews", "spots"
   add_foreign_key "reviews", "users"
   add_foreign_key "spots", "users"
+  add_foreign_key "weathers", "spots"
 end
