55f6f06fd3bb633956292e09b97cd7b424af0669
Thibaud Marcé <thibaud.marce@gmail.com>
2017-11-20 15:06:23 -0500

    added column date to weather

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118230907) do
+ActiveRecord::Schema.define(version: 20171120200307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 20171118230907) do
     t.string   "water_temp"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "date"
     t.index ["spot_id"], name: "index_weathers_on_spot_id", using: :btree
   end
 
