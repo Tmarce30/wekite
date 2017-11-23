34a3bd83faf20ef1c03b5bd7c73c6fea032e8bd5
Thibaud Marcé <thibaud.marce@gmail.com>
2017-11-23 13:59:18 -0500

    average review displayed

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -33,12 +33,54 @@ class Spot < ApplicationRecord
     avg
   end
 
-  def average_for_each_review
+  def level_average_rating
     reviews = self.reviews
     if reviews.count > 0
       total_rating = 0
       reviews.each do |review|
-        total_rating += (review.level_rating + review.environment_rating + review.ambience_rating + review.access_rating).to_f / 4
+        total_rating += review.level_rating
+      end
+      avg = total_rating / reviews.count
+    else
+      avg = 0
+    end
+    avg
+  end
+
+  def environment_average_rating
+    reviews = self.reviews
+    if reviews.count > 0
+      total_rating = 0
+      reviews.each do |review|
+        total_rating += review.environment_rating
+      end
+      avg = total_rating / reviews.count
+    else
+      avg = 0
+    end
+    avg
+  end
+
+  def ambience_average_rating
+    reviews = self.reviews
+    if reviews.count > 0
+      total_rating = 0
+      reviews.each do |review|
+        total_rating += review.ambience_rating
+      end
+      avg = total_rating / reviews.count
+    else
+      avg = 0
+    end
+    avg
+  end
+
+  def access_average_rating
+    reviews = self.reviews
+    if reviews.count > 0
+      total_rating = 0
+      reviews.each do |review|
+        total_rating += review.access_rating
       end
       avg = total_rating / reviews.count
     else
